ef6b9052afb0365b7acbeca423129e44bc6ff89e
Nastya <gamauv@yandex.com>
2019-12-05 15:42:24 +0300

    new features

--- task_041219/twoSum_test.go
+++ task_041219/twoSum_test.go
@@ -16,7 +16,7 @@ var tests = []test{
 	{[]int{0, 1, 2, 3, 999, 9}, []int{0, 5}, 9},
 	{[]int{0, 1, 2, 3, 4, 5}, []int{0, 3}, 3},
 	{[]int{0, 11, 12, 343, 4}, []int{0, 3}, 343},
-	{[]int{9, 9, 3}, []int{0, 2}, 12},
+	{[]int{9, 9, 3}, []int{1, 2}, 12}, //broken answer
 
 }
 
@@ -26,6 +26,10 @@ func TestTwoSum (t *testing.T) {
 		result := twoSum(pairs.input, pairs.target)
 		if !reflect.DeepEqual(result, pairs.wait) {
 			t.Error("Error in", pairs.input, "Im waiting", pairs.wait, "but i have only", result)
+		}
+		// else does not work wtf
+		if reflect.DeepEqual(result, pairs.wait){
+			t.Log(pairs.input, "is correct! Expect:", pairs.wait, "Result:", result)
 		}
 	}
 }
