431aa36d6aa680fe7050b4093bbd52126b919e2a
Nastya <gamauv@yandex.com>
2019-12-05 11:58:18 +0300

    add test(dont work)

--- task 04.12.19/twoSum.go
+++ task 04.12.19/twoSum.go
@@ -2,12 +2,11 @@ package main
 
 import "fmt"
 
-
 func twoSum(nums []int, target int) []int {
 
-	var result = []int{}
+	var result []int
 
-	for i := 0; i < len(nums) - 1; i++{
+	for i := 0; i < len(nums) - 1; i++ {
 		for j := i + 1; j < len(nums); j++ {
 			if nums[i] + nums[j] == target {
 				result = append(result, i, j)
@@ -19,12 +18,6 @@ func twoSum(nums []int, target int) []int {
 }
 
 func main() {
-
-//Given nums = [2, 7, 11, 15], target = 9
-
-arr := []int{0, 2, 3, 6, 2, 2, 2, 7, 11, 15, 9}
-
-fmt.Println(twoSum(arr, 9))
-
+	var i = []int{1, 2, 3, 4}
+	fmt.Println(twoSum(i, 4))
 }
-
