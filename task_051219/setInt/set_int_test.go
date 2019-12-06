6695ed73c08a0920b369d23d800ef9320a47ad9d
Nastya <gamauv@yandex.com>
2019-12-06 12:53:50 +0300

    adding new features

---
+++ task_051219/setInt/set_int_test.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"testing"
+)
+
+type testingS struct {
+	num       int
+	out       bool
+	operation string
+	operationCode int
+}
+
+var tests = []testingS{
+
+	{10, true,"Adding", 1},
+	{20, true,"Adding", 1},
+	{10, true,"Deleting", 2},
+	{10, true,"Deleting", 2},
+	{20, true,"Checking", 3},
+
+}
+
+func TestSetInt(t *testing.T) {
+	sInt := intStruct {
+	elements:  make(map[int]bool),
+	}
+	for _, pairs := range tests {
+		var result bool
+		switch pairs.operationCode {
+		case 1: result = sInt.addElem(pairs.num)
+		case 2: result = sInt.deleteElem(pairs.num)
+		case 3: result = sInt.checkElem(pairs.num)
+		}
+		if result != pairs.out {
+			t.Error("Error!",  pairs.operation, pairs.num, "was incorrect in", sInt.elements)
+			continue
+		}
+		t.Log("Success!", pairs.operation, pairs.num, "was correct! Result:", sInt.elements)
+	}
+}
