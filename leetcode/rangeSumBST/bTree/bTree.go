94cca732827bce8ecb75e5f2ca0159e956bf2862
solympe <gamauv@yandex.com>
2019-12-16 19:32:07 +0300

    rewrite comments

--- leetcode/rangeSumBST/bTree/bTree.go
+++ leetcode/rangeSumBST/bTree/bTree.go
@@ -1,34 +1,35 @@
 package bTree
 
+// BTree represents interface of binary tree struct
 type BTree interface {
 	GetVal() int
 	ShowLeft() *BTree
 	ShowRight() *BTree
 }
 
-//treeNode is a definition for a binary tree node
+// treeNode is a definition for a binary tree node
 type treeNode struct {
 	val   int
 	left  *BTree
 	right *BTree
 }
 
-//GetVal returns value of node
+// GetVal returns value of node
 func (t *treeNode) GetVal() int {
 	return t.val
 }
 
-//ShowLeft returns pointer to left node
+// ShowLeft returns pointer to left node
 func (t *treeNode) ShowLeft() *BTree {
 	return t.left
 }
 
-//ShowRight returns pointer to right node
+// ShowRight returns pointer to right node
 func (t *treeNode) ShowRight() *BTree {
 	return t.right
 }
 
-//NewNode returns new btree node with input parameters
+// NewNode returns new btree node with input parameters
 func NewNode(tleft *BTree, tright *BTree, val int) BTree {
 	return &treeNode{
 		val:   val,
