0bd83b1c8df31de07282f9dd2e7f88b4bf5158c2
Nastya <gamauv@yandex.com>
2019-12-17 15:50:02 +0300

    interselect 1.0

--- main.go
+++ main.go
@@ -3,16 +3,12 @@ package main
 import (
 	"fmt"
 	s "github.com/solympe/Golang_Training/leetcode/interselect/slice"
-	ss "github.com/solympe/Golang_Training/leetcode/interselect/SliceSolver"
 )
 
 func main() {
+	var first = s.NewSlice([]int{1, 2, 2, 1, 4, 6})
+	var second = s.NewSlice([]int{2, 5, 4, 6})
 
-	var first = s.NewSlice([]int{1,2,2,1})
-	var second = s.NewSlice([]int{2})
-
-
-
-	result := s.Intersect(ss.SliceSolver.GetSlice(first), ss.SliceSolver.GetSlice(second))
+	result := s.Intersect(first.GetSlice(), second.GetSlice())
 	fmt.Println(result)
 }
