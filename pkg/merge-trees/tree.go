4e2d9ec0fb9beab12274d2a8173313f1b4fd1d9c
Nastya <gamauv@yandex.com>
2020-01-14 16:36:04 +0300

    codestyle fix

--- pkg/merge-trees/tree.go
+++ pkg/merge-trees/tree.go
@@ -6,6 +6,25 @@ type tree struct {
 	right TreeMerger
 }
 
+// MergeTrees ...
+func (t *tree) MergeTrees(t1 TreeMerger, t2 TreeMerger) TreeMerger {
+	if t1 == nil && t2 == nil {
+		return nil
+	}
+	if t1 == nil {
+		return t2
+	}
+
+	if t2 == nil {
+		return t1
+	}
+	t1.setVal(t2.GetVal() + t1.GetVal())
+	t1.setLeft(t1.MergeTrees(t1.GetLeft(), t2.GetLeft()))
+	t1.setRight(t1.MergeTrees(t1.GetRight(), t2.GetRight()))
+
+	return t1
+}
+
 // GetLeft ...
 func (t *tree) GetLeft() TreeMerger {
 	return t.left
@@ -31,25 +50,6 @@ func (t *tree) setRight(node TreeMerger) {
 
 func (t *tree) setVal(val int) {
 	t.val = val
-}
-
-// MergeTrees ...
-func (t *tree) MergeTrees(t1 TreeMerger, t2 TreeMerger) TreeMerger {
-	if t1 == nil && t2 == nil {
-		return nil
-	}
-	if t1 == nil {
-		return t2
-	}
-
-	if t2 == nil {
-		return t1
-	}
-	t1.setVal(t2.GetVal() + t1.GetVal())
-	t1.setLeft(t1.MergeTrees(t1.GetLeft(), t2.GetLeft()))
-	t1.setRight(t1.MergeTrees(t1.GetRight(), t2.GetRight()))
-
-	return t1
 }
 
 // NewTreeMerger ...
