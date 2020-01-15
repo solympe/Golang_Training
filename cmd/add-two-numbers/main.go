8bb9f92e91e505ddcc1cc024ad4baa117f4e88ad
Nastya <gamauv@yandex.com>
2020-01-15 16:08:02 +0300

    refactor

--- cmd/add-two-numbers/main.go
+++ cmd/add-two-numbers/main.go
@@ -3,17 +3,17 @@ package main
 import (
 	"fmt"
 
-	tree "github.com/solympe/Golang_Training/pkg/add-two-numbers"
+	list "github.com/solympe/Golang_Training/pkg/add-two-numbers"
 )
 
 func main() {
-	list6 := tree.NewListNode(5, nil)
-	list5 := tree.NewListNode(5, list6)
-	list4 := tree.NewListNode(5, list5)
+	list6 := list.NewListNode(5, nil)
+	list5 := list.NewListNode(5, list6)
+	list4 := list.NewListNode(5, list5)
 
-	list3 := tree.NewListNode(5, nil)
-	list2 := tree.NewListNode(5, list3)
-	list1 := tree.NewListNode(5, list2)
+	list3 := list.NewListNode(5, nil)
+	list2 := list.NewListNode(5, list3)
+	list1 := list.NewListNode(5, list2)
 
 	res := list1.AddTwoNumbers(list1, list4)
 	for {
